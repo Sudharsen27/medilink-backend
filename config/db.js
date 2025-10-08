1b3bfbf6a4163f268685e8350c5643fcace2dadb
Sudharsen27 <sundarlingam272000@gmail.com>
2025-10-08 16:12:22 +0530

    clean code

--- config/db.js
+++ config/db.js
@@ -1,24 +1,4 @@
-// // config/db.js
-// const { Pool } = require('pg');
-// require('dotenv').config();
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST || 'localhost',
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT || 5432,
-// });
-
-// // Test connection when starting the server
-// pool.connect()
-//   .then(() => console.log('✅ PostgreSQL connected'))
-//   .catch(err => {
-//     console.error('❌ PostgreSQL connection error:', err.message);
-//     process.exit(1); // stop the app if DB is unreachable
-//   });
-
-// module.exports = pool;
 // config/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
