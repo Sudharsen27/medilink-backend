3bd328742f79a18933aeee043e551c272275da31
Sudharsen27 <sundarlingam272000@gmail.com>
2025-10-22 17:58:39 +0530

    Set personal Git identity for backend

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Routess
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/users', usersRoutes);
