b388db2d2a4ca01333086287bc1cfed8289612f1
Sudharsen27 <sundarlingam272000@gmail.com>
2026-01-09 15:42:25 +0530

    update auth for photo user

--- routes/auth.js
+++ routes/auth.js
@@ -232,6 +232,7 @@ router.post("/login", async (req, res) => {
         name: user.name,
         email: user.email,
         role: user.role,
+        photo: user.photo,
       },
     });
   } catch (err) {
